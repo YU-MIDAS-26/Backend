3090c72cf6ac7083010ff280f4923cd2238c6e61
김민형 <mh706kmh@naver.com>
2026-05-19 14:04:31 +0900

    [추가] 가격(price) 도메인 및 KAMIS 시세 수집 스케줄러 구현

---
+++ spring-server/src/main/java/com/bsight/springserver/domain/price/dto/PriceCollectResult.java
@@ -0,0 +1,10 @@
+package com.bsight.springserver.domain.price.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PriceCollectResult {
+    private int savedCount;
+}
