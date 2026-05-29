eb1a4a5bd4270fd8c3b7a22b96711f708b5e1665
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-30 06:42:39 +0900

    [수정] 아이디 기준 회원 조회 메서드 추가

--- spring-server/src/main/java/com/bsight/springserver/domain/user/repository/UserRepository.java
+++ spring-server/src/main/java/com/bsight/springserver/domain/user/repository/UserRepository.java
@@ -4,9 +4,13 @@ import com.bsight.springserver.domain.user.entity.User;
 import com.bsight.springserver.domain.user.entity.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmailAndStatusNot(String email, UserStatus status);
 
     boolean existsByStudentIdAndStatusNot(String studentId, UserStatus status);
+
+    Optional<User> findByStudentIdAndStatusNot(String studentId, UserStatus status);
 }
