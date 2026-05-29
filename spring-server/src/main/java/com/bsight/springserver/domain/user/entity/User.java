a87ffda11bc90b1e3a08044516fadf9d3c30eb7b
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-30 06:29:51 +0900

    [추가] 회원 기본정보 저장을 위한 User 엔티티 추가

---
+++ spring-server/src/main/java/com/bsight/springserver/domain/user/entity/User.java
@@ -0,0 +1,78 @@
+package com.bsight.springserver.domain.user.entity;
+
+import com.bsight.springserver.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_users_email", columnNames = "email"),
+                @UniqueConstraint(name = "uk_users_student_id", columnNames = "student_id")
+        }
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(name = "student_id", nullable = false, length = 50)
+    private String studentId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean agreedToTerms1;
+
+    @Column(nullable = false)
+    private Boolean agreedToTerms2;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private UserStatus status;
+
+    public static User createStepOneUser(
+            String name,
+            LocalDate birthDate,
+            String email,
+            String phoneNumber,
+            String studentId,
+            String encodedPassword,
+            Boolean agreedToTerms1,
+            Boolean agreedToTerms2
+    ) {
+        User user = new User();
+        user.name = name;
+        user.birthDate = birthDate;
+        user.email = email;
+        user.phoneNumber = phoneNumber;
+        user.studentId = studentId;
+        user.password = encodedPassword;
+        user.agreedToTerms1 = agreedToTerms1;
+        user.agreedToTerms2 = agreedToTerms2;
+        user.status = UserStatus.PENDING_BUSINESS;
+
+        return user;
+    }
+}
