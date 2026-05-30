a816b43753a37dfca055381abb8b1dec8cc21125
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-30 23:38:56 +0900

    [추가] 직원 근무시간 등록 요청 DTO 추가

---
+++ spring-server/src/main/java/com/bsight/springserver/domain/attendance/dto/request/EmployeeAttendanceCreateRequest.java
@@ -0,0 +1,29 @@
+package com.bsight.springserver.domain.attendance.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record EmployeeAttendanceCreateRequest(
+
+        @NotNull(message = "직원 ID를 입력해 주세요.")
+        Long employeeId,
+
+        @NotNull(message = "근무 날짜를 입력해 주세요.")
+        LocalDate workDate,
+
+        @NotNull(message = "출근 시간을 입력해 주세요.")
+        LocalTime checkInTime,
+
+        @NotNull(message = "퇴근 시간을 입력해 주세요.")
+        LocalTime checkOutTime,
+
+        @NotNull(message = "휴게시간 적용 여부를 선택해 주세요.")
+        Boolean breakTimeApplied,
+
+        LocalTime breakStartTime,
+
+        LocalTime breakEndTime
+) {
+}
