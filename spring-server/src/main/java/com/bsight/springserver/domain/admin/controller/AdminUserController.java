511ccb637accec85919bc073388d79f58eb3abaf
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-30 23:33:28 +0900

    [추가] 관리자 전체 회원 목록 조회 API 추가

--- spring-server/src/main/java/com/bsight/springserver/domain/admin/controller/AdminUserController.java
+++ spring-server/src/main/java/com/bsight/springserver/domain/admin/controller/AdminUserController.java
@@ -3,6 +3,7 @@ package com.bsight.springserver.domain.admin.controller;
 import com.bsight.springserver.domain.admin.dto.request.AdminUserRejectRequest;
 import com.bsight.springserver.domain.admin.dto.response.AdminPendingUserResponse;
 import com.bsight.springserver.domain.admin.dto.response.AdminUserApprovalResponse;
+import com.bsight.springserver.domain.admin.dto.response.AdminUserListResponse;
 import com.bsight.springserver.domain.admin.service.AdminUserService;
 import com.bsight.springserver.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,10 +20,19 @@ public class AdminUserController {
 
     private final AdminUserService adminUserService;
 
+    @Operation(summary = "관리자 전체 회원 목록 조회")
+    @GetMapping
+    public ApiResponse<List<AdminUserListResponse>> getUsers() {
+        List<AdminUserListResponse> response = adminUserService.getUsers();
+
+        return ApiResponse.success("회원 목록 조회에 성공했습니다.", response);
+    }
+
     @Operation(summary = "관리자 승인 대기 회원 목록 조회")
     @GetMapping("/pending")
     public ApiResponse<List<AdminPendingUserResponse>> getPendingApprovalUsers() {
         List<AdminPendingUserResponse> response = adminUserService.getPendingApprovalUsers();
+
         return ApiResponse.success("승인 대기 회원 목록 조회에 성공했습니다.", response);
     }
 
@@ -32,6 +42,7 @@ public class AdminUserController {
             @PathVariable Long userId
     ) {
         AdminUserApprovalResponse response = adminUserService.approveUser(userId);
+
         return ApiResponse.success("회원 승인이 완료되었습니다.", response);
     }
 
@@ -42,6 +53,7 @@ public class AdminUserController {
             @Valid @RequestBody AdminUserRejectRequest request
     ) {
         AdminUserApprovalResponse response = adminUserService.rejectUser(userId, request);
+
         return ApiResponse.success("회원 가입 요청이 거절되었습니다.", response);
     }
 }
