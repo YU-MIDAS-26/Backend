1a0916140d760b19ea8466b18c6f3ea4bab819c1
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-06-03 16:30:42 +0900

    [수정] 매출 날짜와 주기 기준 조회 삭제 메서드 추가

--- spring-server/src/main/java/com/bsight/springserver/domain/sales/repository/SalesRepository.java
+++ spring-server/src/main/java/com/bsight/springserver/domain/sales/repository/SalesRepository.java
@@ -1,11 +1,17 @@
 package com.bsight.springserver.domain.sales.repository;
 
+import com.bsight.springserver.common.enums.CycleType;
 import com.bsight.springserver.domain.sales.entity.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface SalesRepository extends JpaRepository<Sales, Long> {
     Optional<Sales> findBySaleDate(LocalDate saleDate);
+
+    List<Sales> findBySaleDateAndCycleType(LocalDate saleDate, CycleType cycleType);
+
+    void deleteBySaleDateAndCycleType(LocalDate saleDate, CycleType cycleType);
 }
